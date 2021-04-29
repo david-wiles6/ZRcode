fcf60541fc4c6eb0ade8483d74eb7d257d1807ba
david-wiles6 <31745593+david-wiles6@users.noreply.github.com>
2021-04-29 16:54:15 -0400

    Create debugging.c

---
+++ Basics/debugging.c
@@ -0,0 +1,9 @@
+//Much like everything else in c, debugging is SUPER weird, it takes some getting used to 
+//but it makes sense eventually
+
+//Debugging is used to print variables and messages to the chat
+//it uses the syntax: DEBUG(("message"));
+// if you want to print a variable to the chat, it's a bit different from other coding
+//languages 
+//you will need to use an escape character % and then a letter associated with the variable type
+//floats are %f booleans are %b and ints are %d
