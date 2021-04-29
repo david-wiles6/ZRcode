e4a774c404c3b82dcf18776fdaeaea5e3fd5c554
david-wiles6 <31745593+david-wiles6@users.noreply.github.com>
2021-04-29 16:46:45 -0400

    Create Pointers.c

---
+++ Basics/Pointers.c
@@ -0,0 +1,27 @@
+// Here we are talking about a special way to use less RAM, as well as make it easier to look at specific elements in an array called pointers
+// Pointers are defined: #define (name) (&(where to point to))
+//for example
+
+#define myPosition (&myState[0])
+
+//note that you don't need a semicolon
+//this points to the array myState, which is going to be a 12 element array that defines a variety of information about 
+//your sphere, and the first 3 elements of this (0-2) are your position
+//instead of creating an array for your position and updating the array each time the loop runs,
+//you can tell the program that whenever you reference that term, look at the other array
+
+//tldr; myPosition will always be equal to the first 3 elements of myState
+
+//define myState
+float myState;
+
+void init(){
+  
+}
+
+void loop(){
+  //store your information in myState, which will also 
+  api.getMyZRState(myState);
+  
+  DEBUG(("%f, %f, %f", myPosition[0], myPosition[1], myPosition[2]));
+}
