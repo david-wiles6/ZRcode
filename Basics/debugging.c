f9486624c0922b7144a05c76c8c6a2b23d6e5248
david-wiles6 <31745593+david-wiles6@users.noreply.github.com>
2021-05-10 10:39:47 -0400

    Update debugging.c

--- Basics/debugging.c
+++ Basics/debugging.c
@@ -6,4 +6,13 @@
 // if you want to print a variable to the chat, it's a bit different from other coding
 //languages 
 //you will need to use an escape character % and then a letter associated with the variable type
-//floats are %f booleans are %b and ints are %d
+//floats are %f booleans are %b and ints are %d, then for each escaped character, you have to put a variable
+//of the same type after the message, demarcated by commas
+//like this:
+float myVariable;
+
+myVariable = 3.0f;
+
+DEBUG(("%f", myVariable));
+
+//this will output 3.0 to the console in the simulation since that is the value of myVariable
